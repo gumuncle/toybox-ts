54a3568adc9be456a3c23d3203732f27bb0063cc
yusuke furukawa <yusuke.furukawa@chatwork.com>
2024-04-12 17:05:39 +0900

    add gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { Faker, ja } from "@faker-js/faker";
+import { fakerJA } from "@faker-js/faker";
 
 console.log("Hello, Bun!");
 
-export const faker = new Faker({
-  locale: [ja],
-});
+export const faker = fakerJA;
 
 console.log(faker.internet.userName());
+console.log(faker.commerce.department());
 
 [...Array(100)].forEach((_) => {
   console.log(faker.person.lastName(), faker.person.firstName());
+  console.log(faker.company.name());
+  console.log(faker.commerce.department());
 });
